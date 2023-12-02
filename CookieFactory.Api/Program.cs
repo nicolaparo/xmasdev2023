51c7064c2d3ad2e7016494ee23996fc5e72bede1
Nicola Paro <nicola.paro@beantech.it>
2023-12-02 17:52:18 +0100

    initial commit

---
+++ CookieFactory.Api/Program.cs
@@ -0,0 +1,25 @@
+
+namespace CookieFactory.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
